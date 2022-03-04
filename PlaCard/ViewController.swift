9370db52e76c0bd689ba3ecb6c053921341eee7c
karenenoukemeu <karenenoukemeu@gmail.com>
2022-03-04 10:56:42 -0600

    First PlaCard Lab done.

--- PlaCard/ViewController.swift
+++ PlaCard/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backLabel: UILabel!
+    
+    @IBOutlet weak var frontLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapOnPlacard(_ sender: Any) {
+        
+        frontLabel.isHidden = true
+    }
+    
 }
 
